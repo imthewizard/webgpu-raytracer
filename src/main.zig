cef8376ef0ee1cd19ee51c437d90fef746fa4479
Pedro Luiz <62962668+imthewizard@users.noreply.github.com>
2026-07-30 20:22:18 -0300

    clearing screen color successfully
    very messy code

--- src/main.zig
+++ src/main.zig
@@ -11,6 +11,10 @@ const WGPUError = error {
     FailedToGetDevice,
 };
 
+var global_device: c.WGPUDevice = undefined;
+var global_surface: c.WGPUSurface = undefined;
+var global_queue : c.WGPUQueue = undefined;
+
 pub fn main() !void {
     const descriptor: c.WGPUInstanceDescriptor = .{
         .requiredFeatureCount = 1,
@@ -39,13 +43,90 @@ pub fn main() !void {
 
         .uncapturedErrorCallbackInfo = .{.nextInChain = null, .callback = uncapturedErrorCallbackInfo, .userdata1 = null, .userdata2 = null},
     };
-    const device: c.WGPUDevice = try requestDeviceSync(instance, adapter, &device_descriptor);
-    defer c.wgpuDeviceRelease(device);
+    // const device: c.WGPUDevice = try requestDeviceSync(instance, adapter, &device_descriptor);
+    // defer c.wgpuDeviceRelease(device);
+    global_device = try requestDeviceSync(instance, adapter, &device_descriptor);
+    defer c.wgpuDeviceRelease(global_device);
+
+
+    // const queue: c.WGPUQueue = c.wgpuDeviceGetQueue(global_device);
+    global_queue = c.wgpuDeviceGetQueue(global_device);
+    defer c.wgpuQueueRelease(global_queue);
+
+    global_surface = getSurface(instance, adapter, global_device);
+    defer c.wgpuSurfaceRelease(global_surface);
+    defer c.wgpuSurfaceUnconfigure(global_surface);
     // could release adapter here
+
+    c.emscripten_set_main_loop(loop, 0, true);
+
+    std.log.info("Finished", .{});
+}
+
+fn loop() callconv(.c) void {
+    var surface_tex: c.WGPUSurfaceTexture = .{
+        .nextInChain = null,
+        .texture = null,
+        .status = 0,
+    };
+    c.wgpuSurfaceGetCurrentTexture(global_surface, &surface_tex);
+
+    if (surface_tex.status == c.WGPUSurfaceGetCurrentTextureStatus_SuccessOptimal) {
+        const target_view: c.WGPUTextureView = c.wgpuTextureCreateView(
+            surface_tex.texture,
+            &c.WGPUTextureViewDescriptor{
+                .nextInChain = null,
+                .label = zig2wgpuString("Surface tex view"),
+                .format = c.WGPUTextureFormat_Undefined,
+                .dimension = c.WGPUTextureViewDimension_2D,
+                .baseMipLevel = 0,
+                .mipLevelCount = c.WGPU_MIP_LEVEL_COUNT_UNDEFINED,
+                .baseArrayLayer = 0,
+                .arrayLayerCount = c.WGPU_ARRAY_LAYER_COUNT_UNDEFINED,
+                .aspect = c.WGPUTextureAspect_Undefined,
+                .usage = c.WGPUTextureUsage_None,
+            }
+        );
+        defer c.wgpuTextureRelease(surface_tex.texture);
+        defer c.wgpuTextureViewRelease(target_view);
+
+        const encoder: c.WGPUCommandEncoder = c.wgpuDeviceCreateCommandEncoder(global_device, null);
+        defer c.wgpuCommandEncoderRelease(encoder);
+
+        // Render pass
+        const render_pass_desc: c.WGPURenderPassDescriptor = .{
+            .nextInChain = null,
+            .label = .{.data = null, .length = 0},
+            .colorAttachmentCount = 1,
+            .colorAttachments = &c.WGPURenderPassColorAttachment{
+                .nextInChain = null,
+                .view = target_view,
+                .depthSlice = c.WGPU_DEPTH_SLICE_UNDEFINED,
+                .resolveTarget = null,
+                .loadOp = c.WGPULoadOp_Clear,
+                .storeOp = c.WGPUStoreOp_Store,
+                .clearValue = c.WGPUColor{.r = 1.0, .g = 0.8, .b = 0.55, .a = 1.0},
+            },
+            .depthStencilAttachment = null,
+            .occlusionQuerySet = null,
+            .timestampWrites = null,
+        };
+        const render_pass: c.WGPURenderPassEncoder = c.wgpuCommandEncoderBeginRenderPass(encoder, &render_pass_desc);
+        defer c.wgpuRenderPassEncoderRelease(render_pass);
+        c.wgpuRenderPassEncoderEnd(render_pass);
+
+        const cmd_buffer = c.wgpuCommandEncoderFinish(encoder, null);
+        defer c.wgpuCommandBufferRelease(cmd_buffer);
+        c.wgpuQueueSubmit(global_queue, 1, &cmd_buffer);
+    }
 }
 
 fn wgpuString2Zig(wgpu_str: c.WGPUStringView) []const u8 {
     return wgpu_str.data[0..wgpu_str.length];
+}
+
+fn zig2wgpuString(zig_str: []const u8) c.WGPUStringView {
+    return .{.data = zig_str.ptr, .length = zig_str.len};
 }
 
 fn requestAdapterSync(inst: c.WGPUInstance) WGPUError!c.WGPUAdapter {
@@ -133,4 +214,40 @@ fn requestDeviceSync(inst: c.WGPUInstance, adapter: c.WGPUAdapter, descriptor: *
 fn uncapturedErrorCallbackInfo(device: [*c]const c.WGPUDevice, err_type: c.WGPUErrorType, message: c.WGPUStringView, _:?*anyopaque, _:?*anyopaque) callconv (.c) void {
     _ = device;
     std.log.err("Error in device: WGPUErrorType {d}, msg: {s}", .{err_type, wgpuString2Zig(message)});
+}
+
+fn getSurface(instance: c.WGPUInstance, adapter: c.WGPUAdapter, device: c.WGPUDevice) c.WGPUSurface {
+    var ext: c.WGPUEmscriptenSurfaceSourceCanvasHTMLSelector = .{
+        .chain = .{.next = null, .sType = c.WGPUSType_EmscriptenSurfaceSourceCanvasHTMLSelector},
+        .selector = zig2wgpuString("canvas"),
+    };
+    const surface: c.WGPUSurface = c.wgpuInstanceCreateSurface(instance,
+        &c.WGPUSurfaceDescriptor{
+            .label = zig2wgpuString("Surface"),
+            .nextInChain = &ext.chain,
+        },
+    );
+
+    var width: c_int = undefined;
+    var height: c_int = undefined;
+    _ = c.emscripten_get_canvas_element_size("#canvas", &width, &height);
+
+    var surface_capabilities: c.WGPUSurfaceCapabilities = .{.nextInChain = null};
+    _ = c.wgpuSurfaceGetCapabilities(surface, adapter, &surface_capabilities);
+    defer c.wgpuSurfaceCapabilitiesFreeMembers(surface_capabilities);
+
+    c.wgpuSurfaceConfigure(surface, &c.WGPUSurfaceConfiguration{
+        .nextInChain = null,
+        .device = device,
+        .format = surface_capabilities.formats[0],
+        .usage = c.WGPUTextureUsage_RenderAttachment,
+        .width = @intCast(width),
+        .height = @intCast(height),
+        .viewFormatCount = 0,
+        .viewFormats = null,
+        .alphaMode = c.WGPUCompositeAlphaMode_Auto,
+        .presentMode = c.WGPUPresentMode_Fifo,
+    });
+
+    return surface;
 }
