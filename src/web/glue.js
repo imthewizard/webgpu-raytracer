480249a14615b62e87766979186a4e91f731cdd3
Pedro Luiz <62962668+imthewizard@users.noreply.github.com>
2026-08-04 20:37:27 -0300

    rendering texture from compute shader to triangle

--- src/web/glue.js
+++ src/web/glue.js
@@ -6,7 +6,9 @@ let presentationFormat
 
 const canvas = document.getElementById("canvas")
 
+let computeModule
 let baseShaderModule
+let screen_texture
 
 export async function initWebGPU() {
 	if (!navigator.gpu) {
@@ -29,7 +31,14 @@ export async function initWebGPU() {
 		}
 	})
 
+	screen_texture = device.createTexture({
+		size: [640, 480],
+		format: "rgba8unorm",
+		usage: GPUTextureUsage.STORAGE_BINDING | GPUTextureUsage.TEXTURE_BINDING | GPUTextureUsage.RENDER_ATTACHMENT,
+	})
+
 	baseShaderModule = await loadShader("./triangle.wgsl")
+	computeModule = await loadShader("./compute.wgsl")
 
 	presentationFormat = navigator.gpu.getPreferredCanvasFormat()
 
@@ -52,47 +61,100 @@ async function loadShader(file) {
 	return device.createShaderModule({ label: file, code: code })
 }
 
+function invokeComputeShader() {
+	const module = computeModule
+
+	const pipeline = device.createComputePipeline({
+		label: "compute",
+		layout: "auto",
+		compute: {
+			module,
+		},
+	})
+
+	const bindGroup = device.createBindGroup({
+		label: "compute bindGroup",
+		layout: pipeline.getBindGroupLayout(0),
+		entries: [
+			{ binding: 0, resource: screen_texture },
+		],
+	})
+
+	const encoder = device.createCommandEncoder({
+		label: "compute encoder",
+	})
+	const pass = encoder.beginComputePass({
+		label: "compute pass",
+	})
+	pass.setPipeline(pipeline)
+	pass.setBindGroup(0, bindGroup)
+	pass.dispatchWorkgroups(640 / 16, 480 / 16) // hardcoded
+	pass.end()
+
+	const commandBuffer = encoder.finish()
+	device.queue.submit([commandBuffer])
+}
+
+function invokeVertFragShader() {
+	const module = baseShaderModule
+
+	const pipeline = device.createRenderPipeline({
+		label: "triangle",
+		layout: "auto",
+		vertex: {
+			entryPoint: "vertexMain",
+			module,
+		},
+		fragment: {
+			entryPoint: "fragmentMain",
+			module,
+			targets: [{ format: presentationFormat }],
+		},
+	})
+
+	const bindGroup = device.createBindGroup({
+		label: "vertfrag bindGroup",
+		layout: pipeline.getBindGroupLayout(0),
+		entries: [
+			{ binding: 0, resource: screen_texture.createView() },
+			{ binding: 1, resource: device.createSampler() },
+		],
+	})
+
+	const renderPassDescriptor = {
+		label: "canvas renderPass",
+		colorAttachments: [
+			{
+				clearValue: [0.3, 0.3, 0.3, 1],
+				loadOp: "clear",
+				storeOp: "store",
+			},
+		],
+	}
+	renderPassDescriptor.colorAttachments[0].view = context.getCurrentTexture().createView()
+
+	const encoder = device.createCommandEncoder({label: "encoder"})
+
+	const pass = encoder.beginRenderPass(renderPassDescriptor)
+	pass.setPipeline(pipeline)
+	pass.setBindGroup(0, bindGroup)
+	pass.draw(3)
+	pass.end()
+
+	const commandBuffer = encoder.finish()
+	device.queue.submit([commandBuffer])
+}
+
 export const importObject = {
 	env: {
 		js_print_str: js_print_str,
 		drawTriangle: function() {
-			const module = baseShaderModule
-
-			const pipeline = device.createRenderPipeline({
-				label: "triangle",
-				layout: "auto",
-				vertex: {
-					entryPoint: "vertexMain",
-					module,
-				},
-				fragment: {
-					entryPoint: "fragmentMain",
-					module,
-					targets: [{ format: presentationFormat }],
-				},
-			});
-
-			const renderPassDescriptor = {
-				label: "canvas renderPass",
-				colorAttachments: [
-					{
-						clearValue: [0.3, 0.3, 0.3, 1],
-						loadOp: "clear",
-						storeOp: "store",
-					},
-				],
-			};
-			renderPassDescriptor.colorAttachments[0].view = context.getCurrentTexture().createView()
-
-			const encoder = device.createCommandEncoder({label: "encoder"})
-
-			const pass = encoder.beginRenderPass(renderPassDescriptor)
-			pass.setPipeline(pipeline)
-			pass.draw(3);
-			pass.end();
-
-			const commandBuffer = encoder.finish()
-			device.queue.submit([commandBuffer])
+			// (async() => {
+			// 	await invokeComputeShader()
+			// 	await invokeVertFragShader()
+			// })
+			invokeComputeShader()
+			invokeVertFragShader()
 		},
 	}
 }
